1ece1980a800ab186921af8b63b7c2c7437c41d8
David Zheng <zhengdav@msu.edu>
2020-03-07 21:28:34 -0800

    added 'addComment' function

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -3,16 +3,27 @@ import React, { useState } from 'react';
 const AddCommentForm = () => {
   const [username, setUsername] = useState('');
   const [commentText, setCommentText] = useState('');
+
+  const addComment = async () => {
+    const result = await fetch(`/api/articles/${articleName}/add-comment`, {
+      method: 'post',
+      body: JSON.stringify({username, text: commentText}),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+  }
+
   return (
     <div id="add-comment-form">
       <h3>Add a Comment</h3>
       <label>
         Name:
-        <input type="text"/>
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)}/>
       </label>
       <label>
         Comment:
-        <textarea rows="4" cols="50"/>
+        <textarea rows="4" cols="50" value={commentText} onChange={e => setCommentText(e.target.value)}/>
       </label>
       <button>Add Comment</button>
     </div>
